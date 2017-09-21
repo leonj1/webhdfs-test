ae36c896b3b460365714ae4b6edc735f74dcdccf
Jose Leon <leonj1@gmail.com>
2017-09-21 00:46:21 -0400

    Got greedy route match working

--- server.go
+++ server.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"crypto/sha256"
 	"encoding/json"
+	"flag"
 	"github.com/gorilla/mux"
 	"github.com/orcaman/concurrent-map"
-	"log"
-	"net/http"
 	"io/ioutil"
+	log "github.com/sirupsen/logrus"
+	"net/http"
 	"fmt"
-	"gopkg.in/natefinch/lumberjack.v2"
+	"os"
 )
 
 //func Run(addr string, sslAddr string, ssl map[string]string, insecureHandler, secureHandler http.Handler) chan error {
@@ -58,9 +58,56 @@ type MyConcurrentMap struct {
 	cMap *cmap.ConcurrentMap
 }
 
+/**
+	Listing a directory /foo
+	curl -i "http://host:port/webhdfs/v1/foo/?op=LISTSTATUS"
+ */
+
+/**
+	Renaming the file /foo/bar to /foo/bar2
+	curl -i -X PUT "http://host:port/webhdfs/v1/foo/bar?op=RENAME&destination=/foo/bar2"
+ */
+
+/**
+	Get Directory Status
+	curl -i "http://localhost:50070/webhdfs/v1/tmp?user.name=istvan&op=GETFILESTATUS"
+	HTTP/1.1 200 OK
+	Content-Type: application/json
+	Expires: Thu, 01-Jan-1970 00:00:00 GMT
+	Set-Cookie: hadoop.auth="u=istvan&p=istvan&t=simple&e=1370210454798&s=zKjRgOMQ1Q3NB1kXqHJ6GPa6TlY=";Path=/
+	Transfer-Encoding: chunked
+	Server: Jetty(6.1.26)
+
+	{
+	  "FileStatus":
+	  {
+		"accessTime"      : 1322596581499,
+		"blockSize"       : 67108864,
+		"group"           : "supergroup",
+		"length"          : 22,
+		"modificationTime": 1322596581499,
+		"owner"           : "szetszwo",
+		"pathSuffix"      : "",
+		"permission"      : "644",
+		"replication"     : 3,
+		"type"            : "FILE"
+	  }
+	}
+*/
+
+type NodeAttr struct {
+}
+
+type FsNode struct {
+	Name       string
+	Type       string
+	Attributes NodeAttr
+}
+
+// GET
 func (m *MyConcurrentMap) secureHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	hash := vars["hash"]
+	hash := vars["path"]
 	w.Header().Set("Content-Type", "application/json")
 	if value, ok := m.cMap.Get(hash); ok {
 		valueAsString := value.(string)
@@ -73,28 +120,37 @@ func (m *MyConcurrentMap) secureHandler(w http.ResponseWriter, r *http.Request) 
 	respondWithJSON(w, 404, response)
 }
 
-func (m *MyConcurrentMap) addHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
+/**
+	Create Directory
+	$ curl -i -X PUT "http://localhost:50070/webhdfs/v1/tmp/webhdfs?user.name=istvan&op=MKDIRS"
+	HTTP/1.1 200 OK
+	Content-Type: application/json
+	Expires: Thu, 01-Jan-1970 00:00:00 GMT
+	Set-Cookie: hadoop.auth="u=istvan&p=istvan&t=simple&e=1370210530831&s=YGwbkw0xRVpEAgbZpX7wlo56RMI=";Path=/
+	Transfer-Encoding: chunked
+	Server: Jetty(6.1.26)
+*/
+func (m *MyConcurrentMap) mkdirsHandler(w http.ResponseWriter, r *http.Request) {
+	// fullUri := r.RequestURI
+	// raqQuery := r.URL.RawQuery
+	vars := mux.Vars(r)
+	path := vars["path"]
+	_, err := ioutil.ReadAll(r.Body)
 	w.Header().Set("Content-Type", "application/json")
 	if err != nil {
-		response := &ErrorResponse{ErrorMessage: "Message not found"}
+		response := &ErrorResponse{ErrorMessage: "Message not found1"}
 		respondWithJSON(w, 404, response)
 		return
 	}
-	payload := &QueryResponse{}
-	err = json.Unmarshal(body, &payload)
-	if err != nil {
-		response := &ErrorResponse{ErrorMessage: "Message not found"}
-		respondWithJSON(w, 404, response)
-		return
-	}
-	h := sha256.New()
-	h.Write([]byte(payload.Message))
-	bar := h.Sum(nil)
-	var sx16 string = fmt.Sprintf("%x", bar)
-	m.cMap.Set(sx16, payload.Message)
-	response := &PostResponse{Digest: sx16}
-	respondWithJSON(w, 201, response)
+	//payload := &QueryResponse{}
+	//err = json.Unmarshal(body, &payload)
+	//if err != nil {
+	//	response := &ErrorResponse{ErrorMessage: "Message not found2"}
+	//	respondWithJSON(w, 404, response)
+	//	return
+	//}
+	//response := "hi"
+	respondWithJSON(w, 201, path)
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
@@ -104,22 +160,34 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Write(response)
 }
 
+func init() {
+	log.SetFormatter(&log.TextFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.WarnLevel)
+}
+
 func main() {
-	log.SetOutput(&lumberjack.Logger{
-		Filename:   "/tmp/tls.log",
-		MaxSize:    5, // megabytes
-		MaxBackups: 3,
-		MaxAge:     3, //days
-	})
-		// Concurrent HashMap
+	port := flag.Int("port", 8080, "listening port")
+	flag.Parse()
+	// Concurrent HashMap
 	bar := cmap.New()
 	foo := &MyConcurrentMap{cMap: &bar}
-	s := mux.NewRouter()
-	s.HandleFunc("/messages", foo.addHandler).Methods("POST")
-	s.HandleFunc("/messages/{hash}", foo.secureHandler).Methods("GET")
+	r := mux.NewRouter()
+	s := r.PathPrefix("/webhdfs/v1/webhdfs").Subrouter()
+	// Create a directory: op=MKDIRS
+	s.HandleFunc(`/{path:.*\/*.*}`, foo.mkdirsHandler).Methods("PUT").Queries("op", "MKDIRS")
+	//s.HandleFunc("/{path}", foo.secureHandler).Methods("GET")
 
-	log.Printf("Staring HTTPS service on %s .../n", ":443")
-	if err := http.ListenAndServeTLS(":443", "localhost.crt", "server.key", s); err != nil {
+	webServerPort := fmt.Sprintf(":%d", *port)
+	log.Info("Staring HTTP service on %s .../n", webServerPort)
+	log.Debug("debug")
+	log.Warn("warn")
+	if err := http.ListenAndServe(webServerPort, s); err != nil {
 		panic(err)
 	}
 }
